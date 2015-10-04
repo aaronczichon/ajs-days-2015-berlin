7db2a56f502bc47a1151ba3fd9bb587d86911382
Aaron Czichon <aaron@inoverse.de>
2015-10-04 22:58:01 +0200

    Ionic Action Sheet

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('workshop', ['ionic', 'workshop.controllers'])
+angular.module('workshop', ['ionic', 'workshop.controllers', 'workshop.cars'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,16 @@
-angular.module('workshop.controllers', [])
+angular.module('workshop.controllers', ['ionic'])
 
-.controller('DaimlerCtrl', function($scope) {
-  
-});
+	.controller('DaimlerCtrl', function ($scope, $ionicActionSheet, $timeout, Cars) {
+		$scope.show = function () {
+			$ionicActionSheet.show({
+				buttons: [
+					{ text: 'Hinzufügen' }
+				],
+				titleText: 'Daimler Optionen',
+				cancelText: 'Abbrechen',
+				buttonClicked: function (index) {
+					return true;
+				}
+			});
+		};
+	});
