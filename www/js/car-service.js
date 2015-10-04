b06dd016c958f91426ec7e45675f5091bb19eefe
Aaron Czichon <aaron@inoverse.de>
2015-10-04 22:04:08 +0200

    Cars App + Daimler Tab + Azure Mobile Services

---
+++ www/js/car-service.js
@@ -0,0 +1,36 @@
+angular.module('workshop.cars', [])
+	.factory('Cars', [function () {
+		// Azure Mobile Service Instanz
+		var client = new WindowsAzure.MobileServiceClient(
+			"https://workshop.azure-mobile.net/",
+			"tjdZsYmoIUmsCGqLnyiIhyxfMKTxfZ98"
+			);
+		var daimlerTable = client.getTable('daimler');
+
+		var getAll = function () {
+			var task = $q.defer();
+			daimlerTable.read().then(function (items) {
+				task.resolve(items);
+			});
+
+			return task.promise;
+		}
+
+		var addItem = function (item) {
+			var task = $q.defer();
+			daimlerTable.insert(item).then(function(){
+				task.resolve();
+			});
+			
+			return task.promise;
+		}
+
+		return {
+			all: function () {
+				return getAll();
+			},
+			push: function (item) {
+				return addItem(item);
+			}
+		}
+	}]);
